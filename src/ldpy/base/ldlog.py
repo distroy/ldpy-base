ad5f928221e478916a47f664cbf58e9f1aa7e90d
distroy <distroy@live.com>
2026-07-21 20:48:43 +0800

    feat(ldlog): add param 'logger'

--- src/ldpy/base/ldlog.py
+++ src/ldpy/base/ldlog.py
@@ -8,19 +8,34 @@
 import logging
 import time
 import traceback
-from typing import Optional
+from typing import Mapping, Optional
+try:
+    from typing import Protocol
+except:
+    from typing_extensions import Protocol
+
+
+class Logger(Protocol):
+    def log(self, level: 'int', msg: 'object', *args: 'object', exc_info: 'Optional[Exception]' = None,
+            stack_info: 'bool' = False, stacklevel: 'int' = 1, extra: 'Optional[Mapping[str, object]]' = None):
+        ...
 
 
 class WithLog(object):
-    def __init__(self, name: str, logging=True, level=logging.INFO, ignore_exc: bool = False):
+    def __init__(self, name: str, logging=True, level=logging.INFO, ignore_exc: bool = False,
+                 logger: 'Optional[Logger]' = None):
         self._name = name
         self._begin = 0
         self._ignore_exc = ignore_exc
         self._logging = logging
         self._level = level
+        self._logger = logger
 
     def _log_func(self, msg: 'str', stacklevel: 'int', exc_info: 'Optional[Exception]' = None):
-        if self._logging:
+        if self._logger:
+            self._logger.log(self._level, msg, stacklevel=stacklevel + 1,
+                             exc_info=exc_info)
+        elif self._logging:
             logging.log(self._level, msg, stacklevel=stacklevel + 1,
                         exc_info=exc_info)
         elif exc_info:
@@ -44,7 +59,9 @@ class WithLog(object):
 if __name__ == "__main__":
     logging.basicConfig(level=logging.INFO,
                         format='%(asctime)s - %(filename)s:%(lineno)d - %(levelname)s - %(message)s')
-    with WithLog('abc', logging=False, ignore_exc=True):
+    with WithLog('logging=1,ignore_exc=1', logging=False, ignore_exc=True):
         1 / 0
-    with WithLog('abc', ignore_exc=True):
+    with WithLog('ignore_exc=1', ignore_exc=True):
+        1 / 0
+    with WithLog('ignore_exc=1,logger=logging', logger=logging, ignore_exc=True):
         1 / 0
